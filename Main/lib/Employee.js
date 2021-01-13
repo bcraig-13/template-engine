8f4c598b5a5e2ae7281de83a4d74bfa4d58e926c
Brandon Craig <thegamingraven13@gmail.com>
2021-01-13 09:34:51 -0800

    Started making employee class on employee.js

--- Main/lib/Employee.js
+++ Main/lib/Employee.js
@@ -1,8 +1,18 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor() {
-        
-    }
+  constructor() {
+    this.name = name;
+    // name
+    this.id = id;
+    // id
+    this.email = email;
+    // email
+    
+    // getName()
+    // getId()
+    // getEmail()
+    // getRole() // Returns 'Employee'
+  }
 }
 
 module.exports = Employee;
